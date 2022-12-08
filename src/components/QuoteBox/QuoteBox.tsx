da3d81ff41ec0e9f6adfef058b14c655f1b73094
Pere <peretin2004@gmail.com>
2022-12-09 00:01:22 +0100

    Tweet intent fix

--- src/components/QuoteBox/QuoteBox.tsx
+++ src/components/QuoteBox/QuoteBox.tsx
@@ -6,34 +6,48 @@ import styles from './QuoteBox.scss';
 
 export interface QuoteBoxProps {
   content: string;
-  author:string;
-  onClickFunc:MouseEventHandler
-  myColor:string;
+  author: string;
+  onClickFunc: MouseEventHandler
+  myColor: string;
 }
 
-export function QuoteBox({content,author,onClickFunc,myColor}: QuoteBoxProps) {
 
-  return (<div 
-  className="QuoteBox" 
-  id="quote-box">
-    
+export function QuoteBox({ content, author, onClickFunc, myColor }: QuoteBoxProps) {
+  function textToTweetIntent(text: string): string {
+    let tweetText: string = text;
+    tweetText = tweetText.replace(/ /g, "%20");
+
+    return tweetText;
+  }
+  console.log(typeof (content));
+  let tweetContent: string = "text=";
+  try {
+    tweetContent += textToTweetIntent(content);
+    tweetContent+="- "
+    tweetContent+=textToTweetIntent(author);
+  } catch (error) { }
+
+  return (<div
+    className="QuoteBox"
+    id="quote-box">
+
     <div className='wrapper1'>
-      <h1 id="text" style={{color:myColor}}>{content}</h1>
+      <h1 id="text" style={{ color: myColor }}>{content}</h1>
       <div className="author">
-      <p id="author" style={{color:myColor}}>{author}</p>
+        <p id="author" style={{ color: myColor }}>{author}</p>
       </div>
     </div>
     <div className="wrapper2">
       <div className="iconsWrapper">
-      <ShareQuoteIcon id="tweet-quote" icon={faTwitterSquare} href={"twitter.com/intent/tweet"} myColor={myColor}/>
-      <ShareQuoteIcon id="tumblr-quote" icon={faTumblrSquare} href={"https://*.tumblr.com/post/*"} myColor={myColor}/>
+        <ShareQuoteIcon id="tweet-quote" icon={faTwitterSquare} href={"https://twitter.com/intent/tweet?" + tweetContent} myColor={myColor} />
+        <ShareQuoteIcon id="tumblr-quote" icon={faTumblrSquare} href={"https://*.tumblr.com/post/*"} myColor={myColor} />
       </div>
       <div className="newQuote">
-        <button id="new-quote" onClick={onClickFunc} style={{backgroundColor:myColor}}>New Quote</button>
+        <button id="new-quote" onClick={onClickFunc} style={{ backgroundColor: myColor }}>New Quote</button>
       </div>
 
     </div>
 
-  
+
   </div>);
 }
